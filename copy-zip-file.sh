4d3609899652c1ced099a1951d94dcac80c50873
Uttam Baral <97355759+baraluttam@users.noreply.github.com>
2022-12-11 09:03:32 +0000

    well let's try it out

--- copy-zip-file.sh
+++ copy-zip-file.sh
@@ -1,24 +1,16 @@
-!/bin/bash
+#!/bin/bash
+
 HOST="ftp.zip-codes.com"
 USER="liveauctioneers"
-PASSWD="673v6B@xzuxJ"
+PASSWORD="673v6B@xzuxJ"
 
-SERVER="ftp.zip-codes.com"
-ID="liveauctioneers"
-PASSWD="673v6B@xzuxJ"
+SOURCE=$"ZIP-STANDARD"
+ALL_FILES="*STANDARD-sql.zip"
 
-expect <<EOF 
-spawn ftp $SERVER 
-expect ": "
-send $ID\r"
-expect "Password:"
-send $PASSWD\r"
-expect "ftp>"
-send "cd ZIP-STANDARD\r"
-expect "ftp>"
-send "prompt\r"
-expect "ftp>"
-send "mget ./ZIP-STANDATD/*.sql\"
-expect "ftp>"
-send "bye\r"
+ftp -inv $HOST <<EOF
+user $USER $PASSWORD
+cd $SOURCE
+prompt
+mget $ALL_FILES
+bye
 EOF
