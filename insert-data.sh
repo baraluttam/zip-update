6087c8b955deb01c84653843dc49d99d9584bc4b
Uttam Baral <97355759+baraluttam@users.noreply.github.com>
2022-12-12 22:20:26 +0000

    let's hope this works

--- insert-data.sh
+++ insert-data.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 DBHost=database-1.ck1n4yzwola8.us-east-2.rds.amazonaws.com
 echo "importing zip-codes-database-MYSQL-STANDARD.sql"
-mysql --host=$DBHost --user=$DBUSERNAME --password=$DBPASSWORD Validation < /Users/uttambaral/code/zipcodes/*MYSQL-STANDARD.sql
+mysql --host=$DBHost --user=$DBUSERNAME --password=$DBPASSWORD Validation < *MYSQL-STANDARD.sql
 echo "finished importing zip-codes-database-MYSQL-STANDARD.sql"
 echo "importing zip-codes-database-STANDARD.sql"
-mysql --host=$DBHost --user=$DBUSERNAME --password=$DBPASSWORD Validation < /Users/uttambaral/code/zipcodes/*database-STANDARD.sql
+mysql --host=$DBHost --user=$DBUSERNAME --password=$DBPASSWORD Validation < *database-STANDARD.sql
 echo "finished imporitng zip-codes-database-STANDARD.sql"
