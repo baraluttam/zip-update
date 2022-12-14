c7c3fc66beb06535cadd463953da57d16f383421
Uttam Baral <97355759+baraluttam@users.noreply.github.com>
2022-12-14 01:50:07 +0000

    let's see

--- dropandcopytable.sh
+++ dropandcopytable.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #this is to copy table from validation.tmp to validation.
-mysql --host=database-1.ck1n4yzwola8.us-east-2.rds.amazonaws.com  --user=admin --password=123456789 -e source ./dropandcopytables.sql
+mysql --host=database-1.ck1n4yzwola8.us-east-2.rds.amazonaws.com  --user=admin --password=123456789 -e < ./dropandcopytables.sql
 
