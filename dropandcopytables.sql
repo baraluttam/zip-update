32970f9eae636ba0105ca06e6d4147f7bdecdf6f
Uttam Baral <97355759+baraluttam@users.noreply.github.com>
2022-12-14 21:18:52 +0000

    let's test this out

--- dropandcopytables.sql
+++ dropandcopytables.sql
@@ -1,4 +1,4 @@
 -- this will remove and copy from validation_tmp to validation
-DROP TABLE IF EXISTS Validation.ZIPCodes;
-CREATE TABLE Validation.ZIPCodes LIKE Validation_tmp.ZIPCodes;
-INSERT Validation.ZIPCodes SELECT * FROM Validation_tmp.ZIPCodes;
+DROP TABLE IF EXISTS Validation_tmp.ZIPCodes;
+CREATE TABLE Validation_tmp.ZIPCodes LIKE TEST_Validation_tmp.ZIPCodes;
+INSERT Validation_tmp.ZIPCodes SELECT * FROM TEST_Validation_tmp.ZIPCodes;
--- insert-data.sh
+++ insert-data.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
                  #change this to Stage DB or Prod DB end point.
 echo "importing zip-codes-database-MYSQL-STANDARD.sql"
-mysql --host=$DBHOST --user=$DBUSERNAME --password=$DBPASSWORD Validation_tmp < *MYSQL-STANDARD.sql
+mysql --host=$DBHOST --user=$DBUSERNAME --password=$DBPASSWORD TEST_Validation_tmp < *MYSQL-STANDARD.sql
 echo "finished importing zip-codes-database-MYSQL-STANDARD.sql"
 echo "importing zip-codes-database-STANDARD.sql"
-mysql --host=$DBHOST --user=$DBUSERNAME --password=$DBPASSWORD Validation_tmp < *database-STANDARD.sql
+mysql --host=$DBHOST --user=$DBUSERNAME --password=$DBPASSWORD TEST_Validation_tmp < *database-STANDARD.sql
 echo "finished imporitng zip-codes-database-STANDARD.sql"
