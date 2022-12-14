fb84a1995139015075e801211221b7d9a86fefde
Uttam Baral <97355759+baraluttam@users.noreply.github.com>
2022-12-14 16:08:29 +0000

    let's test this out

--- sqlquary.sh
+++ sqlquary.sh
@@ -4,6 +4,3 @@ DROP TABLE IF EXISTS Validation.ZIPCodes;
 CREATE TABLE Validation.ZIPCodes LIKE Validation_tmp.ZIPCodes;
 INSERT Validation.ZIPCodes SELECT * FROM Validation_tmp.ZIPCodes;
 EOF
-
-
-
