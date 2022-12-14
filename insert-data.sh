aadfc969f58777ad97d5e57cb12055a4ae0fd707
Uttam Baral <97355759+baraluttam@users.noreply.github.com>
2022-12-14 20:20:19 +0000

    adding variables

--- insert-data.sh
+++ insert-data.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-DBUSERNAME=admin               #we will have to put this in circle ci environment variable
-DBPASSWORD=123456789           #we will have to put this in circle ci environment variable
-DBHost=database-1.ck1n4yzwola8.us-east-2.rds.amazonaws.com                    #change this to Stage DB or Prod DB end point.
+                 #change this to Stage DB or Prod DB end point.
 echo "importing zip-codes-database-MYSQL-STANDARD.sql"
-mysql --host=$DBHost --user=$DBUSERNAME --password=$DBPASSWORD Validation_tmp < *MYSQL-STANDARD.sql
+mysql --host=$DBHOST --user=$DBUSERNAME --password=$DBPASSWORD Validation_tmp < *MYSQL-STANDARD.sql
 echo "finished importing zip-codes-database-MYSQL-STANDARD.sql"
 echo "importing zip-codes-database-STANDARD.sql"
-mysql --host=$DBHost --user=$DBUSERNAME --password=$DBPASSWORD Validation_tmp < *database-STANDARD.sql
+mysql --host=$DBHOST --user=$DBUSERNAME --password=$DBPASSWORD Validation_tmp < *database-STANDARD.sql
 echo "finished imporitng zip-codes-database-STANDARD.sql"
--- sqlquary.sh
+++ sqlquary.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mysql --host=database-1.ck1n4yzwola8.us-east-2.rds.amazonaws.com  --user=admin --password=123456789 <<EOF
+mysql --host=$DBHOST --user=$DBUSERNAME --password=$DBPASSWORD <<EOF
 DROP TABLE IF EXISTS Validation.ZIPCodes;
 CREATE TABLE Validation.ZIPCodes LIKE Validation_tmp.ZIPCodes;
 INSERT Validation.ZIPCodes SELECT * FROM Validation_tmp.ZIPCodes;
