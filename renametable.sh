109d145015063a61d95b72f13b34b31e9eca3a72
Uttam Baral <97355759+baraluttam@users.noreply.github.com>
2023-01-06 19:30:46 +0000

    with hour minute and seconds

--- renametable.sh
+++ renametable.sh
@@ -2,7 +2,7 @@
 #currently using this but we can also use dropandcopytables.sql
 mysql --host=$DBHOST --user=$DBUSERNAME --password=$DBPASSWORD <<EOF
 
-SET @current_date = DATE_FORMAT(NOW(), '%Y_%m_%d');
+SET @current_date = DATE_FORMAT(NOW(), '%Y_%m_%d-%H_%i_%s');
 SET @new_table_name = CONCAT('ZIPCodes_', @current_date);
 
 SET @sql = CONCAT('RENAME TABLE Validation.ZIPCodes TO Validation.', @new_table_name);
