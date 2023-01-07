1365d7ed9f0fb2246028d6fe9e03ad8aae8aefcc
Uttam Baral <97355759+baraluttam@users.noreply.github.com>
2023-01-07 23:53:32 +0000

    delete dile

--- sqlquary.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mysql --host=$DBHOST --user=$DBUSERNAME --password=$DBPASSWORD <<EOF
-DROP TABLE IF EXISTS Validation_tmp_1.ZIPCodes;
-CREATE TABLE Validation_tmp_1.ZIPCodes LIKE Validation_tmp.ZIPCodes;
-INSERT Validation_tmp_1.ZIPCodes SELECT * FROM Validation_tmp.ZIPCodes;
-DROP DATABASE IF EXISTS Validation_tmp;
-EOF
