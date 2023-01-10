be60717a420b3fa331410339eb749483b83d1ff8
Uttam Baral <97355759+baraluttam@users.noreply.github.com>
2023-01-10 19:46:38 +0000

    this should work to remove previous tables

---
+++ RemovePreviousTables.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#currently using this but we can also use dropandcopytables.sql
+mysql --host=$DBHOST --user=$DBUSERNAME --password=$DBPASSWORD <<EOF
+
+START TRANSACTION;
+SET @tables = NULL;
+
+SELECT GROUP_CONCAT(table_name) INTO @tables
+FROM information_schema.tables
+WHERE table_schema = 'Validation' AND table_name LIKE 'ZIPCodes_%' AND table_name != 'ZIPCodes';
+
+SET @tables = CONCAT('DROP TABLE ', @tables);
+PREPARE stmt FROM @tables;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+COMMIT;
+
+EOF
